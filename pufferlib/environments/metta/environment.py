13867ebcea65df20b438ccd5636e526f6a06441c
Richard Higgins <richard@softmax.com>
2025-07-22 13:42:40 -0700

    reduce diff

--- pufferlib/environments/metta/environment.py
+++ pufferlib/environments/metta/environment.py
@@ -17,17 +17,11 @@ def env_creator(name='metta'):
 
 def make(name, config='pufferlib/environments/metta/metta.yaml', render_mode='auto', buf=None, seed=0, **kwargs):
     '''Metta creation function'''
-    # Debug: print all kwargs to see what's being passed
-    print(f"DEBUG: make() called with kwargs: {kwargs}")
     
     # Extract expected parameters with defaults
-    ore_reward = kwargs.pop('ore_reward', 0.25)
-    heart_reward = kwargs.pop('heart_reward', 0.5)
-    battery_reward = kwargs.pop('battery_reward', 0.25)
-    
-    # Check if there are any unexpected kwargs
-    if kwargs:
-        print(f"WARNING: Unexpected kwargs passed to make(): {kwargs}")
+    ore_reward = kwargs.pop('ore_reward', 0.17088483842567775)
+    battery_reward = kwargs.pop('battery_reward', 0.9882859711234822)
+    heart_reward = kwargs.pop('heart_reward', 1.0)
     
     OmegaConf.register_new_resolver("div", oc_divide, replace=True)
     cfg = OmegaConf.load(config)
