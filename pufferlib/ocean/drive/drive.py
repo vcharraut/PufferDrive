a7a5bf389484db92ade0f259e37a0aaf20aa2d19
Daphne Cornelisse <cor.daphne@gmail.com>
2025-09-09 16:22:13 -0400

    .gitignore data files

--- pufferlib/ocean/drive/drive.py
+++ pufferlib/ocean/drive/drive.py
@@ -319,7 +319,7 @@ def process_all_maps():
     binary_dir.mkdir(parents=True, exist_ok=True)
 
     # Path to the training data
-    data_dir = Path("data")
+    data_dir = Path("data/train")
     
     # Get all JSON files in the training directory
     json_files = sorted(data_dir.glob("*.json"))
